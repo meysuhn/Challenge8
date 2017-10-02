0a6213ce74a59a8c14f86d01974ba1cb7d0b1eb9
Chris Mason <meysuhn@gmail.com>
2017-10-02 11:51:25 +0100

    Nearly complete.
    One bug to work out with browser-sync and .sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,20 @@
 "use strict";
 /* jshint node: true */
 
-var gulp = require('gulp'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-  sourceMaps = require('gulp-sourcemaps'),
-  rename = require('gulp-rename'),
-     del = require('del');
+var   gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+      sass = require('gulp-sass'),
+sourceMaps = require('gulp-sourcemaps'),
+    rename = require('gulp-rename'),
+       del = require('del'),
+   connect = require('gulp-connect'),
+  imagemin = require('gulp-imagemin'),
+  browserSync = require('browser-sync').create();
 
-
-// At the end:
-  // NOTE unsure if I still need this in this project. Decide at the end.
-  // NOTE re. scripts task ee the link below about the HTML scripts just after the 3:00 mark. You may need to do something here.
-  //see this vid https://teamtreehouse.com/library/gulp-basics/gulp-your-javascript-workflow/using-third-party-gulp-libraries
-  // NOTE re. scripts task the order of .js files in the array does matter. Depending on which js file depends on another. You may need to modify the above later.
-
-// As a developer, I should be able to run the gulp scripts command at the command line to concatenate, minify, and copy all of the project’s JavaScript files into an all.min.js file that is then copied to the dist/scripts folder.
 gulp.task("scripts", function() {
-  gulp.src([
-    'js/global.js',
+  return gulp.src([
+    'js/global.js', // NOTE is global needed here as it's covered by the line below?
     'js/**/*.js'  // This is a globbing pattern, (every .js file in the js folder).
   ])
   .pipe(sourceMaps.init())
@@ -28,46 +23,83 @@ gulp.task("scripts", function() {
   .pipe(gulp.dest('dist/scripts'));
 });
 
-// Don't think the below is working.
-gulp.task("minifyScrips", function() {
-  gulp.src('dist/scripts/all.min.js')
+// Because scripts is a dependency of minifyScripts below when the script command is run minifyScripts will run too.
+gulp.task("minifyScripts", ['scripts'], function() {
+  return gulp.src('dist/scripts/all.min.js')
   .pipe(uglify())
-  .pipe(rename('app.minTest.js')) // NOTE needs to be sep file or not?
   .pipe(gulp.dest('dist/scripts'));
 });
-// NOTE the minify bit needs to run with concatenate
 
-// As a developer, I should be able to run the gulp styles command at the command line to compile the project’s SCSS files into CSS. NOTE still to do: then concatenate and minify into an all.min.css file that is then copied to the dist/styles folder.
+// the gulp-sass module automatically does the concatenation. It only needs the parent sass file that states the imports to do so.
 gulp.task("styles", function() {
- gulp.src("sass/global.scss") // global.scss is there the imports are held
+ return gulp.src("sass/global.scss") // global.scss is where the imports are held
  .pipe(sourceMaps.init()) // creates sourcemap. This is piped to the next sass method where the sass is actually compliled
- .pipe(sass())
+ .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) // option on gulp-sass module to uglify ('compress') the sass
+ .pipe(rename('all.min.css'))
  .pipe(sourceMaps.write('./'))
- .pipe(gulp.dest('dist/css')); // pipe to new css folder
+ .pipe(gulp.dest('dist/styles')) // pipe to new css folder
+ .pipe(browserSync.stream()); // Because Browsersync only cares about your CSS when it's finished compiling - make sure you call .stream() after gulp.dest
 });
 
-
-
-
-// As a developer, I should be able to run the gulp images command at the command line to optimize the size of the project’s JPEG and PNG files, and then copy those optimized images to the dist/content folder.
-
+//The images task written in ES6 syntax.
+gulp.task('images', () => {
+ return gulp.src('images/*') // glob everything in the images folder
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/content'))
+});
 
 // Cleans away all files/folders created by Gulp.
 gulp.task("clean", function() {
   del(['dist']);
 });
 
-// As a developer, I should be able to run the gulp build command at the command line to run the clean, scripts, styles, and images tasks with confidence that the clean task completes before the other commands.
 
-gulp.task("build", ['scripts', 'styles', 'minifyScrips']);
-// NOTE go back over this to complete https://teamtreehouse.com/library/gulp-basics/improving-your-gulp-task-pipelines/putting-multiple-tasks-together
+// // no other tasks depend on the watch method therefore no return statement is necessary.
+// gulp.task("watchFiles", function() {
+//   gulp.watch('sass/**/*.scss', ['styles']); // recompile css if a file change is detected
+//   //gulp.watch('js/**/*.js', ['minifyScripts']); // recompile js if a file change is detected
+// });
+// // NOTE browser cache problem. Not automatically showing new styles in browser. What to do? Ask community tomorrow.
+// // NOTE watchFiles isn't working. Not automatic recompile.
 
-// As a developer, I should be able to run the gulp command at the command line to run the build task and serve my project using a local web server.
-gulp.task("default", ["clean"], function() {
-  gulp.start('build');
+
+gulp.task("build", ["clean"], function() {
+  gulp.start(['minifyScripts', 'styles', 'images']);
+  return gulp.src(['index.html', 'images/**', 'icons/**'], {base: './'})
+    .pipe(gulp.dest('dist'));
+});
+// clean is first completed, then the scripts in the array are ran in the callback
+// since 'scripts' is a dependency of minifyScripts it doesn't need to be stated in the build task. It will run as a result of minifyScriptsbeing run.
+
+
+
+
+
+gulp.task("default", ["serve"], function() {
 });
 // running just the 'gulp' commaned will clear all previous gulp generated files & folders and then build up again from scratch using 'build'
-// NOTE you need to work out the bit to do with watch methods
+  // clean is first completed, then build is ran in the callback
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['build'], function() {
+// a server will be created on port 3000 when serve is ran.
+    browserSync.init({
+        server: "dist"
+    });
+
+    gulp.watch("sass/*", ['build']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+
+
+  // Extra Credit
+  // As a developer, when I run the default gulp command, it should continuously watch for changes to any .scss file in my project. When there is a change to one of the .scss files, the gulp styles command is run and the files are compiled, concatenated, and minified to the dist folder. My project should then reload in the browser, displaying the changes.
+
+
+
+
 
 //-DONE-------------------------------------
 
@@ -77,7 +109,18 @@ gulp.task("default", ["clean"], function() {
 // DONE As a developer, when I run the gulp scripts or gulp styles commands at the command line, source maps are generated for the JavaScript and CSS files respectively.
  // I think this is ot be handled by the scripts and styles commands, not a separate task.
 
+// DONE As a developer, I should be able to run the gulp styles command at the command line to compile the project’s SCSS files into CSS then concatenate and minify into an all.min.css file that is then copied to the dist/styles folder.
+
+// DONE As a developer, I should be able to run the gulp images command at the command line to optimize the size of the project’s JPEG and PNG files, and then copy those optimized images to the dist/content folder.
+
 // DONE As a developer, I should be able to run the gulp clean command at the command line to delete all of the files and folders in the dist folder.
 
-// LESSONS LEARNT
+// DONE As a developer, I should be able to run the gulp scripts command at the command line to concatenate, minify, and copy all of the project’s JavaScript files into an all.min.js file that is then copied to the dist/scripts folder.
+
+// DONE As a developer, I should be able to run the gulp build command at the command line to run the clean, scripts, styles, and images tasks with confidence that the clean task completes before the other commands.
+
+// DONE As a developer, I should be able to run the gulp command at the command line to run the build task and serve my project using a local web server.
+
+
+//-LESSONS LEARNT--------------------------
 // gulpfile.js should always be in the root of the project
